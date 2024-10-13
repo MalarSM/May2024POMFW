2fccce2bbf379f4e87ba002059fb1ba7a81b3ddf
MalarSM <malarvizhi.qa@gmail.com>
2024-10-13 16:46:57 -0700

    test

--- src/main/java/com/qa/opencart/errors/AppError.java
+++ src/main/java/com/qa/opencart/errors/AppError.java
@@ -2,5 +2,5 @@ package com.qa.opencart.errors;
 
 public class AppError {
 
-	public static final String INVALID_BROWSER_MESG = "=====please pass the right browser NaME===";
+	public static final String INVALID_BROWSER_MESG = "=====please pass the right browser Name===";
 }
