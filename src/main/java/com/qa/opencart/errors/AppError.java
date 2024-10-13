8d7d7984ca2de34ea74b20e0d917091aa1677428
MalarSM <malarvizhi.qa@gmail.com>
2024-10-13 16:40:59 -0700

    App error file updated

--- src/main/java/com/qa/opencart/errors/AppError.java
+++ src/main/java/com/qa/opencart/errors/AppError.java
@@ -2,5 +2,5 @@ package com.qa.opencart.errors;
 
 public class AppError {
 
-	public static final String INVALID_BROWSER_MESG = "=====please pass the right browser name===";
+	public static final String INVALID_BROWSER_MESG = "=====please pass the right browser NaME===";
 }
